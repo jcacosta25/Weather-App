d5752d72a1e16df5f424850b96230c724a46efcf
Juan Carlos Acosta <juancacosta25@gmail.com>
2017-05-19 23:21:43 -0500

    DataBases

---
+++ app/src/main/java/com/juancacosta/kotlinweather/data/db/ForecastDbHelper.kt
@@ -0,0 +1,45 @@
+package com.juancacosta.kotlinweather.data.db
+
+import android.database.sqlite.SQLiteDatabase
+import com.juancacosta.kotlinweather.ui.App
+import org.jetbrains.anko.db.AUTOINCREMENT
+import org.jetbrains.anko.db.INTEGER
+import org.jetbrains.anko.db.ManagedSQLiteOpenHelper
+import org.jetbrains.anko.db.PRIMARY_KEY
+import org.jetbrains.anko.db.TEXT
+import org.jetbrains.anko.db.createTable
+import org.jetbrains.anko.db.dropTable
+
+/**
+ * Created by jcacosta on 5/19/17.
+ *
+ */
+class ForecastDbHelper(): ManagedSQLiteOpenHelper(App.instance,ForecastDbHelper.DB_NAME, null,ForecastDbHelper.DB_VERSION){
+    companion object{
+        val DB_NAME = "forecast.db"
+        val DB_VERSION = 1
+        val instance by lazy { ForecastDbHelper() }
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+       db?.createTable(CityForecastTable.NAME,true,
+               CityForecastTable.ID to INTEGER + PRIMARY_KEY,
+               CityForecastTable.CITY to TEXT,
+               CityForecastTable.COUNTRY to TEXT)
+
+        db?.createTable(DayForecastTable.NAME,true,
+                DayForecastTable.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
+                DayForecastTable.DATE to INTEGER,
+                DayForecastTable.DESCRIPTION to TEXT,
+                DayForecastTable.HIGH to INTEGER,
+                DayForecastTable.LOW to INTEGER,
+                DayForecastTable.ICON_URL to TEXT,
+                DayForecastTable.CITY_ID to INTEGER)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        db?.dropTable(CityForecastTable.NAME,true)
+        db?.dropTable(DayForecastTable.NAME,true)
+        onCreate(db)
+    }
+}
